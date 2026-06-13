7daed5e27b7eb0d0a628fae0ba3a0010a93ad73a
papayastorecl <papayastore.cl@gmail.com>
2026-06-13 16:57:46 -0400

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
 'https://cfhjywllsnsurulywoyc.supabase.co';
 
 const SUPABASE_KEY =
-'TU_PUBLISHABLE_KEY';
+'sb_publishable_zJzWm_HJp5ykplTBnH0oiA_dqJbK9T1';
 
 const supabaseClient =
 window.supabase.createClient(
