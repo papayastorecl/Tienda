7281ff829ecd34a0668e2c55f959d2418b09320d
papayastorecl <papayastore.cl@gmail.com>
2026-06-19 15:54:30 -0400

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -9,3 +9,21 @@ window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+
+console.log("✅ Supabase conectado");
+
+async function testSupabase() {
+
+    const { data, error } =
+    await supabaseClient
+        .from('configuracion')
+        .select('*');
+
+    console.log("CONFIG:", data);
+
+    if (error) {
+        console.error("ERROR:", error);
+    }
+}
+
+testSupabase();
